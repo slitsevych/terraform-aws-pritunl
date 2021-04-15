51a1ae6bfca1dd97731263851bd5cb42b3eafbee
Karol Czeryna <k@e-dot.uk>
2021-04-15 15:49:18 +0100

    Update for TF v0.15

--- main.tf
+++ main.tf
@@ -15,14 +15,14 @@ data "aws_ami" "oracle" {
 }
 
 resource "aws_instance" "pritunl" {
-  ami           = data.aws_ami.oracle.id
-  instance_type = var.instance_type
-  key_name      = var.aws_key_name
-  user_data     = file("${path.module}/provision.sh")
+  ami                  = data.aws_ami.oracle.id
+  instance_type        = var.instance_type
+  key_name             = var.aws_key_name
+  user_data            = file("${path.module}/provision.sh")
   iam_instance_profile = var.iam_instance_profile
   root_block_device {
     volume_size           = var.volume_size
-    tags                  = merge(map("Name", format("%s-%s", var.resource_name_prefix, "vpn")), var.tags, )
+    tags                  = merge(tomap({ "Name" = format("%s-%s", var.resource_name_prefix, "vpn") }), var.tags, )
     delete_on_termination = false # we want' to keep our old HD for VPN - better to remove it manually later
   }
 
@@ -39,11 +39,11 @@ resource "aws_instance" "pritunl" {
 
   subnet_id                   = var.public_subnet_id
   associate_public_ip_address = true
-  tags                        = merge(map("Name", format("%s-%s", var.resource_name_prefix, "vpn")), var.tags, )
+  tags                        = merge(tomap({ "Name" = format("%s-%s", var.resource_name_prefix, "vpn") }), var.tags, )
 }
 
 resource "aws_eip" "pritunl" {
   instance = aws_instance.pritunl.id
   vpc      = true
-  tags     = merge(map("Name", format("%s-%s", var.resource_name_prefix, "vpn")), var.tags, )
+  tags     = merge(tomap({ "Name" = format("%s-%s", var.resource_name_prefix, "vpn") }), var.tags, )
 }
--- securitygroups.tf
+++ securitygroups.tf
@@ -44,65 +44,19 @@ resource "aws_security_group" "pritunl" {
     from_port   = -1
     to_port     = -1
     protocol    = "icmp"
-    cidr_blocks = var.internal_cidrs
+    cidr_blocks = "${length(var.internal_cidrs) == 0 ? [data.aws_vpc.selected.cidr_block] : var.internal_cidrs}"
   }
 
   # outbound internet access
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = merge(
-    map("Name", format("%s-%s", var.resource_name_prefix, "vpn")),
+    tomap({"Name" = format("%s-%s", var.resource_name_prefix, "vpn")}),
     var.tags,
   )
 }
-
-# resource "aws_security_group" "allow_from_office" {
-#   name        = "${var.resource_name_prefix}-whitelist"
-#   description = "Allows SSH connections and HTTP(s) connections from office"
-#   vpc_id      = var.vpc_id
-
-#   # SSH access
-#   ingress {
-#     description = "Allow SSH access from select CIDRs"
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = var.whitelist
-#   }
-
-#   # HTTPS access
-#   ingress {
-#     description = "Allow HTTPS access from select CIDRs"
-#     from_port   = 443
-#     to_port     = 443
-#     protocol    = "tcp"
-#     cidr_blocks = var.whitelist
-#   }
-
-#   # ICMP
-#   ingress {
-#     description = "Allow ICMPv4 from select CIDRs"
-#     from_port   = -1
-#     to_port     = -1
-#     protocol    = "icmp"
-#     cidr_blocks = var.whitelist
-#   }
-
-#   # outbound internet access
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   tags = merge(
-#     map("Name", format("%s-%s", var.resource_name_prefix, "whitelist")),
-#     var.tags,
-#   )
-# }
