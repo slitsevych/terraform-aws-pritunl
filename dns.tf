c60037cf89979f607956650b2d68345a7310ab61
Stepan Litsevych <s.litsevych@aimprosoft.com>
2023-06-26 14:28:39 +0300

    updated iam instance profile

--- dns.tf
+++ dns.tf
@@ -8,4 +8,10 @@ resource "aws_route53_record" "vpn" {
   type    = "A"
   ttl     = 300
   records = [aws_instance.pritunl.public_ip]
+
+  lifecycle {
+    ignore_changes = [
+      zone_id
+    ]
+  }
 }
