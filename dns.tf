3de50bb30dff9c50c0dda1b9aab2d50b90b5389d
Stepan Litsevych <s.litsevych@aimprosoft.com>
2023-06-27 14:41:28 +0300

    updated iam instance profile

--- dns.tf
+++ dns.tf
@@ -2,12 +2,31 @@ data "aws_route53_zone" "public_zone" {
   name = var.domain_name
 }
 
+data "aws_route53_zone" "private_zone" {
+  name         = var.domain_name
+  private_zone = true
+}
+
 resource "aws_route53_record" "vpn" {
   zone_id = data.aws_route53_zone.public_zone.zone_id
   name    = "${var.subdomain_prefix}.${data.aws_route53_zone.public_zone.name}"
   type    = "A"
   ttl     = 300
-  records = [aws_instance.pritunl.public_ip]
+  records = [aws_eip.pritunl.public_ip]
+
+  lifecycle {
+    ignore_changes = [
+      zone_id
+    ]
+  }
+}
+
+resource "aws_route53_record" "vpn_private" {
+  zone_id = data.aws_route53_zone.private_zone.zone_id
+  name    = "${var.subdomain_prefix}.${data.aws_route53_zone.private_zone.name}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_eip.pritunl.private_ip]
 
   lifecycle {
     ignore_changes = [
