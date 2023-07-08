5e9cac7aa6be9ced0519cf981260d6f4d67937b5
Stepan Litsevych <s.litsevych@aimprosoft.com>
2023-07-08 15:19:47 +0300

    added wireguard-tools in provision_arm64.sh script

--- scripts/provision_arm64.sh
+++ scripts/provision_arm64.sh
@@ -36,7 +36,7 @@ sudo yum -y groupinstall 'Development Tools'
 sudo yum -y install ipset iptables nano openssl-devel bzip2-devel libffi libffi-devel sqlite-devel xz-devel \
       zlib-devel gcc git openvpn openssl net-tools iptables psmisc ca-certificates \
       selinux-policy selinux-policy-devel wget nano tar policycoreutils-python-utils \
-      bridge-utils
+      bridge-utils wireguard-tools
 
 # Install python
 yum -y install python39 python39-devel python39-pip
